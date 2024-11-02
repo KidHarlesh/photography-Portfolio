a95d7ba0c06406df55d52303effcc890fb7d9a64
KidHarlesh <123456789+odin@users.noreply.github.com>
2024-11-02 16:06:06 -0700

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from "./Components/About.jsx"
 import Booking from "./Components/Booking.jsx"
 import Down from "./Components/Down.jsx"
 import Hero from "./Components/Hero.jsx"
-import Navbar from "./components/Navbar.jsx"
+import Navbar from "./Components/Navbar.jsx"
 import Portfolio from "./Components/Portfolio.jsx"
 import Testimonal from "./Components/Testimonal.jsx"
 
