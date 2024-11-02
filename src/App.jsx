f29c4c773473db399bc0d0c9ad5510d45b35ce1d
KidHarlesh <123456789+odin@users.noreply.github.com>
2024-11-02 15:21:47 -0700

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import About from "./Components/About.jsx";
-import Booking from "./Components/Booking.jsx";
-import Footer from "./Components/Footer.jsx";
+import About from "./Components/About.jsx"
+import Booking from "./Components/Booking.jsx"
+import Down from "./Components/Down.jsx"
 import Hero from "./Components/Hero.jsx"
 import Navbar from "./components/Navbar.jsx"
-import Portfolio from "./Components/Portfolio.jsx";
-import Testimonal from "./Components/Testimonal.jsx";
-
+import Portfolio from "./Components/Portfolio.jsx"
+import Testimonal from "./Components/Testimonal.jsx"
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <About/>
-      <Portfolio/>
-      <Testimonal/>
-      <Booking/>
-      <Footer/>
+      <About />
+      <Portfolio />
+      <Testimonal />
+      <Booking />
+      <Down/>
     </div>
   );
 }
 
-export default App
+export default App 
