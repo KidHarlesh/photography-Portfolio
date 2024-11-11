23a28116a88d77919a663b4c99f62a2f2d5aa56c
KidHarlesh <123456789+odin@users.noreply.github.com>
2024-11-11 04:04:25 -0800

    new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import HeroSection from "../component/HeroSection";
 // import Aboutsection from "../component/Aboutsection";
-// import Portfoliosection from "../component/Portfoliosection";
+import Portfoliosection from "../component/Portfoliosection";
 import Testimonialsection from "../component/Testimonialsection";
 // import Footsection from "../component/Footsection";
 import Navbar from "../component/Navbar";
