b1b841e45cf72ca2382d6e6bb447222fa34cb970
KidHarlesh <123456789+odin@users.noreply.github.com>
2024-11-10 16:13:11 -0800

    new up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import HeroSection from "../component/HeroSection";
 // import Aboutsection from "../component/Aboutsection";
-// import Portfoliosection from "../component/Portfoliosection";
-// import Testimonialsection from "../component/Testimonialsection";
-// import Footsection from "../component/Footsection";
+import Portfoliosection from "../component/Portfoliosection";
+import Testimonialsection from "../component/Testimonialsection";
+import Footsection from "../component/Footsection";
 import Navbar from "../component/Navbar";
 
 const Home = () => {
@@ -11,11 +11,10 @@ const Home = () => {
     <div>
       <Navbar />
       <HeroSection />
-      <HeroSection />
       {/* <Aboutsection /> */}
-      {/* <Portfoliosection />
+      <Portfoliosection />
       <Testimonialsection />
-      <Footsection />  */}
+      <Footsection />
     </div>
   );
 };
