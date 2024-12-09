55ad28ebb5c6712f1673746ca6e8c2af529b3736
Toluwalasefrootify <toluwalasealowoesin@frootify.tech>
2024-12-09 14:48:54 -0800

    new commit

--- src/pages/Porfolio.jsx
+++ src/pages/Porfolio.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Porfolio = () => {
   return (
-    <div className="bg-black h-screen ">
+    <div className="bg-black h-screen  p-10">
       <h1 className="text-white">coming soon.....</h1>
     </div>
   );
