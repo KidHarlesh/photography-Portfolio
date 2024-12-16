7788a0d90bbb9703a4ad2cd98cd6173af57c2b0d
KidHarlesh <alowoesintoluwalase@gmail.com>
2024-12-16 15:09:33 -0800

    new commit

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "../component/Navbar";
+import Testimonialsection from "../component/Testimonialsection";
+import Footer from "../component/Footer";
 
 const AboutMe = () => {
   return (
     <div className="bg-black h-screen ">
-      <Navbar/>
-      <h1 className="text-white text-center pt-28">coming soon.....</h1>
+      <Navbar />
+      <h1 className="text-white text-center pt-60 text-[100px]">Under Construction.....</h1>
+      <Testimonialsection/>
+      <Footer/>
     </div>
   );
 };
--- src/pages/Porfolio.jsx
+++ src/pages/Porfolio.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Navbar from "../component/Navbar";
+import Footer from "../component/Footer";
+import Testimonialsection from "../component/Testimonialsection";
+import FAQuestion from "../component/FAQuestion";
 
 const Porfolio = () => {
   return (
     <div className="bg-black h-screen ">
-      <Navbar/>
-      <h1 className="text-white text-center pt-28">coming soon.....</h1>
+      <Navbar />
+      <h1 className="text-white text-center pt-60 text-[100px]">
+        Under Construction.....
+      </h1>
+      <FAQuestion/>
+      <Testimonialsection />
+      <Footer />
     </div>
   );
 };
