9b82b7ed4b2c94ec57101dd88142fd4cdc4da867
KidHarlesh <alowoesintoluwalase@gmail.com>
2024-12-24 14:52:30 -0800

    few changes

--- src/component/AboutSectionDextop.jsx
+++ src/component/AboutSectionDextop.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const AboutSectionDextop = () => {
   return (
-    <div>AboutSectionDextop</div>
-  )
+    <section>
+      <div>
+        <h1>ABOUT</h1>
+      </div>
+
+    </section>
+  );
 }
 
 export default AboutSectionDextop
