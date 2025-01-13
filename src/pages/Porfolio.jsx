8125213554bb0497afe765e893f420169c3640d3
KidHarlesh <alowoesintoluwalase@gmail.com>
2025-01-13 14:11:00 -0800

    new commit

--- src/pages/Porfolio.jsx
+++ src/pages/Porfolio.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Navbar from "../component/Navbar";
 import Footer from "../component/Footer";
+import Testimonialsection from "../component/Testimonialsection";
+import FAQuestion from "../component/FAQuestion";
 
 const Porfolio = () => {
   return (
     <div className="bg-black h-screen ">
       <Navbar />
-      <h1 className="text-white text-center pt-60 ">
-        Under Construction.....
-      </h1>
+      <h1 className="text-white text-center pt-60 ">Under Construction.....</h1>
+      <FAQuestion />
+      <Testimonialsection />
+
       <Footer />
     </div>
   );
